775224658819e0d44c8d82537c0d87abbb511385
Gabriel Almeida <111467400+GabriielAlmd@users.noreply.github.com>
2023-05-13 01:29:15 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Globalization;
-namespace ConsoleApplication2
+namespace ConsoleAp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x = 10.35784; //declarando variaveis double X int Y .....
-            int y = 32;
-            string z = "Maria";
+            double x = 25.45879; //declarando variaveis double X int Y .....
+            int y = 22;
+            string z = "Marta";
             char w = 'F';
+            
             Console.Write("Olá mundo!"); //ESCREVE SEM QUEBRA DE LINHA  
             Console.WriteLine("Bom dia!"); //ESCREVE QUEBRANDO UMA LINHA ABAIXO 
             Console.WriteLine("Até mais.");
@@ -27,7 +28,16 @@ namespace ConsoleApplication2
             Console.ReadLine();
 
            //===============================================================================
+              string frase = "Gabriel é um otimo programador, e LOGO vai ser um Master SENIOR";
 
+            Console.WriteLine(frase.ToLower());    //comando pra deixar todas as letras minusculas
+            Console.WriteLine(frase.ToUpper());    //comando pra deixar todas as letras maiusculas
+            Console.WriteLine(frase.IndexOf('1'));   //primeira posição do caractere indicado
+            Console.WriteLine(frase.LastIndexOf('G')); //ultima posição do caractere indicado
+            Console.WriteLine(frase.Substring(0, 4));  //recorta a string passando uma posição inicial e final
+            Console.WriteLine(frase.Substring(4));   //recorta a string a partir da posição indicada
+            Console.WriteLine(frase.Replace('g', 'G')); //subistitui um caracter indicado por outro tambem indicado
+            Console.WriteLine(frase.Replace("Gabriel", "Bruno")); //substitui uma palavra por outra
 
         }
     }
